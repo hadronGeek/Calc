b2971fd8ddd3e6afd5dc772e3ca3015b1a718692
hadronGeek <bibiaz52@gmail.com>
2017-01-26 18:01:42 -0500

    Added Constructors, get and set methods.

--- src/Coor.java
+++ src/Coor.java
@@ -5,4 +5,32 @@ public class Coor extends Point
 {
     private double x;
     private double y;
+
+    public Coor(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public double getX()
+    {
+        return x;
+    }
+
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    @Override
+    public double getY()
+    {
+        return y;
+    }
+
+    public void setY(double y)
+    {
+        this.y = y;
+    }
 }
--- src/Polar.java
+++ src/Polar.java
@@ -1,8 +1,36 @@
 /**
  * Created by Beck on 1/26/2017.
  */
-public class Polar
+public class Polar extends Point
 {
     private double mag;
     private double angle;
+
+    public Polar(double mag, double angle)
+    {
+        this.mag = mag;
+        this.angle = angle;
+    }
+
+    @Override
+    public double getMag()
+    {
+        return mag;
+    }
+
+    public void setMag(double mag)
+    {
+        this.mag = mag;
+    }
+
+    @Override
+    public double getAngle()
+    {
+        return angle;
+    }
+
+    public void setAngle(double angle)
+    {
+        this.angle = angle;
+    }
 }
